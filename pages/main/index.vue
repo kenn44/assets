0557559b1cc636fc592ca37666b030e72fd10dba
kenn <kennethassogba@gmail.com>
2019-02-11 02:14:57 +0100

    Try to display notification

--- pages/main/index.vue
+++ pages/main/index.vue
@@ -17,6 +17,7 @@ import AppLogo from "~/components/AppLogo.vue";
 
 export default {
   // middleware: "router-auth",
+  props: ["user"],
   components: {
     AppLogo
   }
--- pages/register/signup.vue
+++ pages/register/signup.vue
@@ -38,6 +38,7 @@
 import Notification from "~/components/Notification";
 
 export default {
+  props: ["error"],
   components: {
     Notification
   },
